f3b4b39e64107e7d03d0773bf8104b2dfc7d6fed
ipurvis <ian.c.purvis@gmail.com>
2017-06-20 13:44:04 -0400

    Implements ActiveModel:Dirty for TestModel

--- spec/test_model.rb
+++ spec/test_model.rb
@@ -1,5 +1,30 @@
+require "active_model"
+
 class TestModel
+  include ActiveModel::Dirty
   include ActiveModel::Validations
   include PoliceState
-  attr_accessor :state
+
+  define_attribute_methods :state
+
+  def initialize(state: nil)
+    @state = state
+  end
+
+  def state
+    @state
+  end
+
+  def state=(val)
+    state_will_change! unless val == @state
+    @state = val
+  end
+
+  def save!
+    changes_applied
+  end
+
+  def reload!
+    clear_changes_information
+  end
 end
