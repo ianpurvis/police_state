6367d0701ba35d2ba222ba145c720171a8038590
ipurvis <ian.c.purvis@gmail.com>
2017-06-27 13:23:44 -0400

    Improves transition helper support for ActiveRecord::Enum attributes

--- lib/police_state/transition_helpers.rb
+++ lib/police_state/transition_helpers.rb
@@ -8,12 +8,26 @@ module PoliceState::TransitionHelpers
   end
 
   def attribute_transitioned?(attr, options={})
+    options = _transform_options_for_attribute(attr, options)
     !!previous_changes_include?(attr) &&
       (!options.include?(:to) || options[:to] == previous_changes[attr].last) &&
       (!options.include?(:from) || options[:from] == previous_changes[attr].first)
   end
 
   def attribute_transitioning?(attr, options={})
+    options = _transform_options_for_attribute(attr, options)
     attribute_changed?(attr, options)
+  end
+
+
+  private
+
+  # Facilitates easier change checking for ActiveRecord::Enum attributes
+  # by casting any symbolized :to and :from values into their native strings.
+  def _transform_options_for_attribute(attr, options={})
+    return options unless self.class.respond_to?(:attribute_types)
+    options.transform_values {|value|
+      self.class.attribute_types.with_indifferent_access[attr].cast(value)
+    }
   end
 end
