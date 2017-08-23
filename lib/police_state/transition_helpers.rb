0de33dc47f52031d94dee1998676a9afc3e9b0ff
Ian Purvis <ian.c.purvis@gmail.com>
2017-08-23 17:12:47 -0400

    Documents the transition helper module

--- lib/police_state/transition_helpers.rb
+++ lib/police_state/transition_helpers.rb
@@ -1,13 +1,35 @@
 module PoliceState
+
+  # == Police State \Transition Helpers
+  #
+  # Provides a way to monitor attribute state transitions for Active Record and Active Model objects.
+  #
+  # If using with Active Model, make sure that your class implements +ActiveModel::Dirty+.
   module TransitionHelpers
     extend ActiveSupport::Concern
     include ActiveModel::AttributeMethods
 
+    # :stopdoc:
     included do
       raise ArgumentError, "Including class must implement ActiveModel::Dirty" unless include?(ActiveModel::Dirty)
       attribute_method_suffix "_transitioned?", "_transitioning?"
     end
+    # :startdoc:
 
+    # Returns +true+ if +attribute+ transitioned at the last save event, otherwise +false+.
+    #
+    # You can specify origin and destination states using the +:from+ and +:to+
+    # options. If +attribute+ is an +ActiveRecord::Enum+, these may be
+    # specified as either symbol or their native enum value.
+    #
+    #  model = Model.create!(status: :complete)
+    #  # => #<Model:0x007fa94844d088 @status=:complete>
+    #
+    #  model.status_transitioned?                            # => true
+    #  model.status_transitioned?(from: nil)                 # => true
+    #  model.status_transitioned?(to: :complete)             # => true
+    #  model.status_transitioned?(to: "complete")            # => true
+    #  model.status_transitioned?(from: nil, to: :complete)  # => true
     def attribute_transitioned?(attr, options={})
       options = _transform_options_for_attribute(attr, options)
       !!previous_changes_include?(attr) &&
@@ -15,6 +37,20 @@ module PoliceState
         (!options.include?(:from) || options[:from] == previous_changes[attr].first)
     end
 
+    # Returns +true+ if +attribute+ is currently transitioning but not saved, otherwise +false+.
+    #
+    # You can specify origin and destination states using the +:from+ and +:to+
+    # options. If +attribute+ is an +ActiveRecord::Enum+, these may be
+    # specified as either symbol or their native enum value.
+    #
+    #  model = Model.new(status: :complete)
+    #  # => #<Model:0x007fa94844d088 @status=:complete>
+    #
+    #  model.status_transitioning?                            # => true
+    #  model.status_transitioning?(from: nil)                 # => true
+    #  model.status_transitioning?(to: :complete)             # => true
+    #  model.status_transitioning?(to: "complete")            # => true
+    #  model.status_transitioning?(from: nil, to: :complete)  # => true
     def attribute_transitioning?(attr, options={})
       options = _transform_options_for_attribute(attr, options)
       attribute_changed?(attr, options)
