1d34df0c6b04548270a9d67288cd7e6f13cc58c1
Ian Purvis <ian.c.purvis@gmail.com>
2017-08-23 19:25:54 -0400

    Formats transition validator with explicit module

--- lib/police_state/transition_validator.rb
+++ lib/police_state/transition_validator.rb
@@ -1,30 +1,32 @@
-class PoliceState::TransitionValidator < ActiveModel::EachValidator # :nodoc:
+module PoliceState
+  class TransitionValidator < ActiveModel::EachValidator # :nodoc:
 
-  def check_validity!
-    raise ArgumentError, "Options must include :from" unless options.include?(:from)
-    raise ArgumentError, "Options must include :to" unless options.include?(:to)
-    raise ArgumentError, "Options cannot specify array for :to" if options[:to].is_a?(Array)
-  end
-
-  def validate_each(record, attr_name, value)
-    unless transition_allowed?(record, attr_name)
-      record.errors.add(attr_name, "can't transition to #{value}", options)
+    def check_validity!
+      raise ArgumentError, "Options must include :from" unless options.include?(:from)
+      raise ArgumentError, "Options must include :to" unless options.include?(:to)
+      raise ArgumentError, "Options cannot specify array for :to" if options[:to].is_a?(Array)
     end
-  end
+
+    def validate_each(record, attr_name, value)
+      unless transition_allowed?(record, attr_name)
+        record.errors.add(attr_name, "can't transition to #{value}", options)
+      end
+    end
 
 
-  private
+    private
 
-  def destination
-    options[:to]
-  end
+    def destination
+      options[:to]
+    end
 
-  def origins
-    options[:from].instance_eval {nil? ? [nil] : Array.wrap(self)}
-  end
+    def origins
+      options[:from].instance_eval {nil? ? [nil] : Array.wrap(self)}
+    end
 
-  def transition_allowed?(record, attr_name)
-    !record.attribute_transitioning?(attr_name, to: destination) ||
-      origins.any? {|origin| record.attribute_transitioning?(attr_name, from: origin)}
+    def transition_allowed?(record, attr_name)
+      !record.attribute_transitioning?(attr_name, to: destination) ||
+        origins.any? {|origin| record.attribute_transitioning?(attr_name, from: origin)}
+    end
   end
 end
